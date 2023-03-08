e9a991859cc9e48629d042b11142c617319575eb
jjm7HWU <jjm7@hw.ac.uk>
2023-03-08 17:15:20 +0000

    create speak-to-chatbot API

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,25 @@ const { dialogueManager, intentRecogniser } = require("../server/objects");
 
 const router = express.Router();
 
+// receive user dialogue and respond
+router.post("/api/speak-to-chatbot", (req, res) => {
+    console.log("API POST: speak-to-chatbot");
+    console.log(req.body);
+    let userSpeech = req.body.value;
+    console.log("USER SPEECH");
+    console.log(userSpeech);
+    intentRecogniser.recogniseIntent(userSpeech, response => {
+        let intentName = response.value;
+        let intent = "U1 " + intentName;
+        dialogueManager.decideAction(intent, response => {
+            console.log("DIAAA RESPONSE");
+            console.log(response);
+            let action = response.value;
+            res.send({ value: `Intent: ${intentName}, Action: ${action}`});
+        });
+    });
+});
+
 // receive a line of speech from the client and respond with
 // the name of the intent recognised
 router.post("/api/recognise-intent", (req, res) => {
